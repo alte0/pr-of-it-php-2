d5efa489499072302dd9fc08a167df4ae4adc266
Максим Дмитриев <makc-dmv@yandex.ru>
2023-11-07 10:56:20 +0500

    fixed edited article

--- 3/edit_article.php
+++ 3/edit_article.php
@@ -12,13 +12,15 @@ if (isset($_GET['id']) && $_GET['id'] > 0) {
 
 if (
     $_SERVER['REQUEST_METHOD'] === 'POST' &&
-    isset($_POST['article']['title']) && isset($_POST['article']['content']) &&
+    isset($_POST['article']['title']) && isset($_POST['article']['content']) && isset($_POST['article']['author']) &&
+    $_POST['article']['author'] > 0 &&
     mb_strlen(trim($_POST['article']['title'])) > 0 && mb_strlen(trim($_POST['article']['content'])) > 0
 ) {
     $article = new \App\Model\Article();
     $article->id = $id;
     $article->title = trim($_POST['article']['title']);
     $article->content = trim($_POST['article']['content']);
+    $article->author_id = intval($_POST['article']['author']);
     $article->save();
 }
 
@@ -33,7 +35,9 @@ if (is_object($article)) {
     $view->h3 = 'Редактирование - ' . $article->title;
     $view->title = $article->title;
     $view->content = $article->content;
+    $view->author_id = $article->author_id;
     $view->textSubmit = 'Изменить новость';
+    $view->autors = \App\Model\Author::findAll();
 
     $layoutView->title = 'Редактирование - ' . $article->title;
 }
--- 3/templates/new_article.php
+++ 3/templates/new_article.php
@@ -26,7 +26,12 @@
             <label for="exampleFormControlSelect1" class="form-label">Автор новости</label>
             <select name="article[author]" class="form-select" id="exampleFormControlSelect1">
             <?php foreach ($this->autors as $author) { ?>
-                <option value="<?php echo $author->id; ?>"><?php echo $author->fio; ?></option>
+                <option 
+                        value="<?php echo $author->id; ?>"
+                        <?php if (isset($this->author_id) && $this->author_id > 0 && $this->author_id == $author->id){ ?>
+                            selected
+                        <?php } ?>
+                ><?php echo $author->fio; ?></option>
             <?php } ?>
             </select>
         </div>
